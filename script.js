76d4844e80fe950c40a3bcec385f1afbe22b4b9d
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-21 12:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function calculateArea() {
-	let radius = prompt("Enter the radius of the circle:");
-	
-	let ans = (Math.PI * radius * radius).toFixed(2)
-     
-	alert(`The area of the circle with radius ${radius} is ${ans}`);
-           
+###function calculateArea() {
+	// Write your code here
+	const radius = prompt("Enter the radius of the circle:");
+    
+	if (!isNaN(radius) && parseFloat(radius) > 0) {
+        const area = Math.PI * parseFloat(radius) * parseFloat(radius);
+        alert(`The area of the circle with radius ${radius} is ${area.toFixed(2)}`);
+    } 
+	else {
+        alert("Invalid input. Please enter a positive number for the radius.");
+    }
 }
+
 calculateArea();
